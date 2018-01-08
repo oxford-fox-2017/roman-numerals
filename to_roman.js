731e27c6eb458c16653eb896612d39e5e2cc0275
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-01-08 17:04:43 +0700

    Roman Progress

---
+++ to_roman.js
@@ -0,0 +1,27 @@
+function to_roman(num) {
+    let tampung = "";
+    let bank = {
+        I: 1,
+        IV: 4,
+        V: 5,
+        IX: 9,
+        X: 10,
+        XL: 50,
+        L: 50,
+        XC: 90,
+        C: 100,
+        CD: 400,
+        D: 500,
+        M: 1000,
+    };
+
+    for (let i = 0; i <= 3000; i++) {
+        if (
+
+        }
+    }
+
+}
+
+
+console.log(to_roman(476))
