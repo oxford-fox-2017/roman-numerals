a2f536d857a13dca98a724ad19e96e05f14025c5
haripermadi <harinurpermadi@gmail.com>
2018-01-08 18:52:31 +0700

    roman numerals problem

---
+++ roman_num.js
@@ -0,0 +1,31 @@
+/*
+buat function to_roman isi parameter dengan 'integer'
+set num isi angka yg mewakili roman num
+set romawi isi dengan roman num dengan urutan yang sama dengan num
+looping num
+jika integer lebih besar atau sama dengan num i maka masukan nilai romawi i ke hasil
+kurangi nilai integer dengan nilai num i 
+*/
+function to_roman(integers){
+const num = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
+const romawi = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+let hasil ='';
+for(let i =0; i<num.length;i++){
+    // console.log(num[i]);
+    // console.log(romawi[i]);
+    while(integers >= num[i]){
+        // console.log(integers);
+        hasil += romawi[i];
+        // console.log(hasil);
+        integers -= num[i];
+        // console.log(integers);
+    }
+}
+return hasil;
+}
+// console.log('1 | I|', to_roman(1));
+ console.log('4 | IV|', to_roman(4));
+ console.log('9 | IX|', to_roman(9));
+console.log('23 | XXIII|', to_roman(23));
+console.log('120 | CXX|', to_roman(120));
